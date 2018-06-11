a999107d35ca321a5c5e6c9f7b2b83f290eae422
Alumnos <alumnos@vh214-14.local>
2018-06-11 08:51:08 -0500

    comió

--- 20plus/ViewController.swift
+++ 20plus/ViewController.swift
@@ -28,6 +28,42 @@ class ViewController: UIViewController {
     
     @IBAction func btnLoginPressed(_ sender: Any) {
         
+        
+        //valido el login
+        //performSegue(withIdentifier: "login", sender: self)
+        
+        
+        //http://vmdev1.nexolink.com:90/TruequeAppAPI/api/UsersApp?username
+        
+        Alamofire.request("http://vmdev1.nexolink.com:90/TruequeAppAPI/api/UsersApp?username="+txtCorreoElcrotnico.text!+"&password="+txtPass.text!).responseJSON { response in
+                print("Request: \(String(describing: response.request))")   // original url request
+                print("Response: \(String(describing: response.response))") // http url response
+                print("Result: \(response.result)")                         // response serialization result
+                
+                if let json = response.result.value {
+                    print("JSON: \(json)") // serialized json response
+                }
+                
+                if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                    print("Data: \(utf8Text)") // original server data as UTF8 string
+                }
+                
+                let sJson = JSON(response.result.value)
+                if(sJson["Id"] != JSON.null){
+                    print(sJson["Name"])
+                    
+                    self.performSegue(withIdentifier: "login", sender: nil)
+                }else{
+                    let alert = UIAlertController(title: "Fatal FAIl!!", message: "ERROR!! ERROR!! ERROR!!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                        NSLog("The \"OK\" alert occured.")
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                }
+        }
+        
+        
+        /*
         Alamofire.request("http://192.168.1.2:9990/api/padres?email="+txtCorreoElcrotnico.text! + "&password="+txtPass.text!).responseJSON { response in
                 print("Request: \(String(describing: response.request))")   // original url request
                 print("Response: \(String(describing: response.response))") // http url response
@@ -54,7 +90,7 @@ class ViewController: UIViewController {
                     }))
                     self.present(alert, animated: true, completion: nil)
                 }
-        }
+        }*/
     }
     
     
