3b6a876e9a26614bd98012408bebe1ecd9368042
Alumnos <alumnos@vh214-14.local>
2018-06-16 08:56:44 -0500

    commit pantallas

---
+++ Entities/profesor.swift
@@ -0,0 +1,13 @@
+//
+//  profesor.swift
+//  20plus
+//
+//  Created by Alumnos on 16/06/18.
+//  Copyright © 2018 renato. All rights reserved.
+//
+
+import UIKit
+
+class profesor: NSObject {
+
+}
